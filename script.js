89af81b461db67eabc2b7f6ce193d0fc1aeb3f8e
Arifeen Hossain <arifeenh18@gmail.com>
2021-08-20 00:28:19 +0600

    added logics

--- script.js
+++ script.js
@@ -11,7 +11,32 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent =
       "You've guessed correctly!";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60B347";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too high!";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "You've lost. Try again!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "You've lost. Try again!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
